1ea1b2656b314a415942614c59a1f4654c2a2a2e
Miguel Fernández <miguelfernandezfernandez@gmail.com>
2019-07-22 02:14:41 +0200

    Implement test for Slack.WritePump

--- cable/slack_test.go
+++ cable/slack_test.go
@@ -58,3 +58,44 @@ WAIT:
 	Equal(t, "freshprince: Sup Jay!", inbox[0].String())
 	Equal(t, "freshprince: Uncle Phil, you here?", inbox[1].String())
 }
+
+func TestSlack_WritePump(t *testing.T) {
+	client := &fakeSlackAPI{}
+
+	fakeSlack := &Slack{
+		relayedChannelID: slackChatID,
+		botUserID:        slackBotID,
+		client:           client,
+		Pump:             NewPump(),
+	}
+
+	fakeSlack.Outbox <- createTelegramMessage("Sup Jay!", "Will", "Smith", "freshprince")
+	fakeSlack.Outbox <- createTelegramMessage(":clap: Psss!", "Will", "Smith", "freshprince")
+
+	fakeSlack.WritePump()
+
+	// wait for the pump to to process the channel up to 1 second, or timeout
+	timeout := time.NewTimer(1 * time.Second)
+
+WAIT:
+	for {
+		select {
+		case <-timeout.C:
+			Fail(t, "timeout while processing the Read Pump")
+			break WAIT
+		default:
+			if len(fakeSlack.Outbox) == 0 {
+				break WAIT
+			}
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
+
+	Equal(t, 2, len(client.sent))
+
+	first := asSlackJSONMessage(client.sent[0])
+	Equal(t, "Sup Jay!", first.Text)
+
+	second := asSlackJSONMessage(client.sent[1])
+	Equal(t, ":clap: Psss!", second.Text)
+}
